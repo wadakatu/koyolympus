c3b4085af8588f1fe7e3e799997aa407af821361
wadakatu <wadakatukoyo330@gmail.com>
2021-01-17 20:43:01 +0900

    install vue

--- koyolympus/routes/web.php
+++ koyolympus/routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any}', function(){
+   return view('index');
+})->where('any', '.*');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
