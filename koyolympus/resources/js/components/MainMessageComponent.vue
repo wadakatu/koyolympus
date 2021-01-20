9d5c46fcbc29eeed522f71c697d8324cebe9a139
wadakatu <wadakatukoyo330@gmail.com>
2021-01-20 22:32:38 +0900

    メインページ左側完成

---
+++ koyolympus/resources/js/components/MainMessageComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <h1>Koyolympus</h1>
+            <p>
+                Photography is a means of capturing and preserving the instant that doesn't come twice.<br>
+                That's why I cannot stop exploring the world with my camera gear to cut out the exact moment
+                that I would like to keep as it is.
+            </p>
+            <button class="button">Explore My Photo</button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+.row {
+    display: flex;
+    height: 80%;
+    align-items: center;
+}
+
+.col {
+    flex-basis: 50%;
+}
+
+h1 {
+    color: #fff;
+    font-size: 85px;
+}
+
+p {
+    color: #fff;
+    font-size: 15px;
+    line-height: 18px;
+}
+
+button {
+    width: 180px;
+    color: #000;
+    font-size: 12px;
+    padding: 12px 0;
+    background: #fff;
+    border: 0;
+    border-radius: 20px;
+    outline: none;
+    margin-top: 30px;
+}
+
+</style>
--- koyolympus/webpack.mix.js
+++ koyolympus/webpack.mix.js
@@ -12,9 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
+    .js("resources/js/router.js", "public/js")
+    .sass('resources/sass/app.scss', 'public/css')
 
-.options({
-  processCssUrls: false
-});
+
+    .options({
+        processCssUrls: false
+    });
 
