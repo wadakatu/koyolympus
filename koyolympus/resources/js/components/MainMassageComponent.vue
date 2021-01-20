2e22d110aafdcc76d23c77ed4bd162662d92e1ba
wadakatu <wadakatukoyo330@gmail.com>
2021-01-20 09:21:25 +0900

    一旦コミット

---
+++ koyolympus/resources/js/components/MainMassageComponent.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="row">
+        <div class="col">
+            <h1>Koyolympus</h1>
+            <p>No one is immortal. No one can put back the clock. Nothing stays the same.
+                That's why people cannot stop taking photography because
+                photography is a means of capturing and preserving the instant that doesn't come twice.
+                Therefore, we explore the world with our camera and chase whatever we love and feel great
+                to cut out the exact moment that we would like to keep as it is.
+            </p>
+        </div>
+        <div class="col">
+
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
