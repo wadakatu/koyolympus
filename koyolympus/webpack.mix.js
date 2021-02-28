65d0ac074d09e7ed7a8bdb926394a0b9aa62f782
wadakatu <wadakatukoyo330@gmail.com>
2021-02-28 21:19:36 +0900

    gzip設定追加

--- koyolympus/webpack.mix.js
+++ koyolympus/webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+const CompressionPlugin = require('compression-webpack-plugin');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -14,8 +14,17 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .js("resources/js/router.js", "public/js")
     .sass('resources/sass/app.scss', 'public/css')
-
-
+    .webpackConfig({
+        plugins: [
+            new CompressionPlugin({
+                filename: '[path].gz[query]',
+                algorithm: 'gzip',
+                test: /\.js$|\.css$|\.html$|\.svg$/,
+                threshold: 10240,
+                minRatio: 0.8,
+            })
+        ]
+    })
     .options({
         processCssUrls: false
     });
